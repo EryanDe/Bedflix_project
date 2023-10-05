8466d26f84c3d093daa4139381bc4142f5f83df1
CamossiJeremy <camossijeremy@gmail.com>
2023-10-05 11:15:29 +0200

    front

---
+++ JS/Carrousel.js
@@ -0,0 +1,18 @@
+const carousel = document.querySelector(".carousel");
+const slides = document.querySelectorAll(".slide");
+
+let currentIndex = 0;
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const translateXValue = -currentIndex * 100;
+    carousel.style.transform = `translateX(${translateXValue}%)`;
+}
+
+setInterval(nextSlide, 4000);
+
+updateCarousel();
